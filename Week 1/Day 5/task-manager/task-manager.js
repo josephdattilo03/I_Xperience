ba2c92635bf8661b58347047613406ba1cdd3aff
josephdattilo03 <jadattilobusiness@gmail.com>
2023-07-17 12:03:17 +0200

    working on day 6

--- Week 1/Day 5/task-manager/task-manager.js
+++ Week 1/Day 5/task-manager/task-manager.js
@@ -1,71 +1,64 @@
-
-
-
-
 class Task {
     constructor(name, description, date, priority) {
-        this.name = name
-        this.description = description
-        this.date = date
-        this.priority = priority
+        this.name = name;
+        this.description = description;
+        this.date = date;
+        this.priority = priority;
+    }
+
+    static fromJSON(json) {
+        return new Task(json.name, json.description, json.date, json.priority);
     }
 }
 
-
-const exampleTask = new Task("example", "this is an example description", "date", "priority")
-
-let tasks = []
-tasks.push(exampleTask)
-tasks.push(new Task("ex2","this is ex2", "date","priority"))
+let tasks = [];
 
 class UI {
     constructor() {
-        this.tableBody = document.getElementById('table')
-        console.log(this.tableBody)
+        this.tableBody = document.getElementById("table");
+        this.form = document.getElementById("form");
+        this.name = document.getElementById("task-name")
+        this.description = document.getElementById("task-description")
+        this.date = document.getElementById("date")
+        this.priority = document.getElementById("priority")
+        this.tasks = []
+        this.renderTable();
     }
 
-    render() {
-        for (let i = 0; i < tasks.length; i++) {
-            const tr = document.createElement("tr")
-            tr.setAttribute("class", "card rounded bg-gray-200 mx-9 py-3 px-9 flex justify-center mb-2 items-center text-xl")
-            const p = document.createElement("p")
-            p.textContent = tasks[i].name
-            p.setAttribute("class", "mr-9 py-1")
-            const button = document.createElement("button")
-            button.setAttribute("class", "border-2 inline border-gray-500 text-white bg-red-500 px-2 py-1 rounded")
-            button.textContent = "Delete"
-            tr.appendChild(p)
-            tr.appendChild(button)
-            this.tableBody.appendChild(tr)
+    renderTable() {
+        this.tableBody.innerHTML = "";
+        for (let i = 0; i < this.tasks.length; i++) {
+            const tr = this.createRow(this.tasks[i]);
+            this.tableBody.appendChild(tr);
         }
     }
-    renderNew() {
-        const tr = document.createElement("tr")
-            tr.setAttribute("class", "card rounded bg-gray-200 mx-9 py-3 px-9 flex justify-center mb-2 items-center text-xl")
-            const p = document.createElement("p")
-            p.textContent = tasks[tasks.length - 1].name
-            p.setAttribute("class", "mr-9 py-1")
-            const button = document.createElement("button")
-            button.setAttribute("class", "border-2 inline border-gray-500 text-white bg-red-500 px-2 py-1 rounded")
-            button.textContent = "Delete"
-            tr.appendChild(p)
-            tr.appendChild(button)
-            this.tableBody.appendChild(tr)
+    renderRow(task) {
+        const tr = document.createElement("tr");
+        tr.setAttribute(
+            "class",
+            "card rounded bg-gray-200 mx-9 py-3 px-9 flex justify-center mb-2 items-center text-xl"
+        );
+        const p = document.createElement("p");
+        p.textContent = task.name;
+        p.setAttribute("class", "mr-9 py-1");
+        const button = document.createElement("button");
+        button.setAttribute(
+            "class",
+            "border-2 inline border-gray-500 text-white bg-red-500 px-2 py-1 rounded"
+        );
+        button.textContent = "Delete";
+        tr.appendChild(p);
+        tr.appendChild(button);
+        return tr;
+    }
+    
+    getTasksLocalStorage() {
+        const json = localStorage.getItem("tasks")
+        if(json) {
+            const arr = JSON.parse(json)
+            this.task = arr.map((x) => {
+                Task.fromJSON(x)
+            })
+        }
     }
 }
-
-
-
-
-
-document.addEventListener("DOMContentLoaded", function(){
-    const ui = new UI()
-    ui.render()
-    document.addEventListener("submit", (e) => {
-        e.preventDefault()
-        const form = document.forms[0]
-        tasks.push(new Task(form.task.value, form.description.value, form.date.value, form.priority.value))
-        ui.renderNew()
-    })
-})
-
